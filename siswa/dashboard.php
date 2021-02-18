f689f7ab28f3f0fb5d30f76084d35e4490956755
Rizkiiwidya <rizkiwidya04@gmail.com>
2021-02-18 17:48:51 +0700

    Cek Judul

--- siswa/dashboard.php
+++ siswa/dashboard.php
@@ -38,7 +38,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Aplikasi Pendaftaran Siswa</div>
+                <div class="sidebar-brand-text mx-3">Aplikasi Pendaftaran Siswa Baru</div>
             </a>
 
             <!-- Heading -->
